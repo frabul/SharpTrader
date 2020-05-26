f87e98bca01f6178978b8316dda84b6fbd36e5a0
F.Buldo <frbuldo@gmail.com>
2020-05-26 21:16:13 +0200

    added BackTestOptimizer project

---
+++ BackTestOptimizer/Program.cs
@@ -0,0 +1,42 @@
+﻿using SharpTrader;
+using SharpTrader.Algos;
+using SharpTrader.Plotting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTestOptimizer
+{
+    class Program
+    {
+        static void ShowPlotCallback(PlotHelper plot)
+        {
+            PlottingHelper.Show(plot); 
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+ 
+
+            Optimizer.Configuration conf = new Optimizer.Configuration
+            {
+                SessionName = "Hpm2",
+                BacktesterConfig = new BackTester.Configuration
+                {
+                    DataDir = @"D:\ProgettiBck\SharpTraderBots\Bin\Data",
+                    HistoryDb = @"D:\ProgettiBck\SharpTraderBots\Bin\Data",
+                    Market = "Binance",
+                    StartingBalance = new AssetAmount("BTC", 100), 
+                    StartTime = new DateTime(2020, 02, 01),
+                    EndTime = new DateTime(2020, 05, 30),
+                    AlgoClass = typeof(HighPassMeanReversionAlgo2).AssemblyQualifiedName,
+                }, 
+            };
+            Optimizer optim = new Optimizer(conf, ShowPlotCallback);
+            optim.Start();
+        } 
+    }
+}
