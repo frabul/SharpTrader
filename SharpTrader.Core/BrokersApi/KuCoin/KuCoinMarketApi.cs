b9f22ed66c81b00060f0996a6b84cc97f340fcf6
Francesco Buldo <francesco.buldo@gmail.com>
2021-12-09 10:02:28 +0100

    start implementation of KuCoin support

---
+++ SharpTrader.Core/BrokersApi/KuCoin/KuCoinMarketApi.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kucoin;
+namespace SharpTrader.Core.BrokersApi.KuCoin
+{
+    public class KuCoinMarketApi
+    {
+    }
+}
---
+++ SharpTrader.Core/BrokersApi/KuCoin/KuCoinTradeBarsRepository.cs
@@ -0,0 +1,17 @@
+﻿using SharpTrader.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpTrader.Core.BrokersApi.KuCoin
+{
+    public class KuCoinTradeBarsRepository : TradeBarsRepository
+    {
+        NLog.Logger Logger = NLog.LogManager.GetLogger(nameof(KuCoinTradeBarsRepository));
+
+        public KuCoinTradeBarsRepository(string dataDir, ChunkFileVersion cv, ChunkSpan chunkSpan) : base(dataDir, cv, chunkSpan)
+        {
+
+        }
+    }
+}
