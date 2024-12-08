c0379ec3e439ef6f91c45b94a779de222e92caf2
Francesco Buldo <francesco.buldo@gmail.com>
2024-12-08 13:00:48 +0100

    Modify/fix TradeBarConsolidator:
    - allow to synch open time on start of day instead of epoch
    - now it emits filler candles
    - fix synchronization on open time

--- SharpTrader.Core/TradeBarConsolidator.cs
+++ SharpTrader.Core/TradeBarConsolidator.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Diagnostics;
 #pragma warning disable CS1998
 namespace SharpTrader
 {
     public class TradeBarConsolidator
     {
+        public bool SynchOnStartOfDay { get; }
         public TimeSpan Resolution { get; }
-      
+        public Candlestick LastEmittedCandle { get; private set; } = new Candlestick();
+        public TimeSpan FinalCandleUpdateTimeout { get; set; } = TimeSpan.FromSeconds(8);
+
         private Candlestick FormingCandle = new Candlestick();
 
         DateTime BaseTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         public event Action<ITradeBar> OnConsolidated;
-        
-        public TradeBarConsolidator(TimeSpan resolution)
+
+        public TradeBarConsolidator(TimeSpan resolution, bool synchOnStartOfDay = false)
         {
+            this.SynchOnStartOfDay = synchOnStartOfDay;
             this.Resolution = resolution;
         }
 
@@ -28,50 +33,66 @@ namespace SharpTrader
                 throw new NotImplementedException();
             }
         }
-        
-        public void Scan(DateTime timeNow)
+
+        private void EmitCandle(ref Candlestick candle)
         {
-            if (!FormingCandle.IsDefault() && timeNow >= FormingCandle.Time)
-            {
-                OnConsolidated?.Invoke(FormingCandle);
-                FormingCandle = default;
-            }
+            OnConsolidated?.Invoke(candle);
+            LastEmittedCandle = candle;
+            FormingCandle = new Candlestick();
         }
 
-        private DateTime GetOpenTime(DateTime timeNow, TimeSpan timeFrame)
+        private DateTime CalculateFirstOpenTime(DateTime timeNow, TimeSpan timeFrame)
         {
-            long resto = (timeNow - BaseTime).Ticks % timeFrame.Ticks;
+            var baseTime = BaseTime;
+            if (SynchOnStartOfDay)
+                baseTime = new DateTime(timeNow.Year, timeNow.Month, timeNow.Day, 0, 0, 0, timeNow.Kind);
+
+
+            long resto = (timeNow - baseTime).Ticks % timeFrame.Ticks;
             return new DateTime((timeNow.Ticks - resto), timeNow.Kind);
         }
-        
+
         private void OnTradeBar(ITradeBar newCandle)
         {
-            DateTime tclose = newCandle.Time;
-            DateTime topen = newCandle.OpenTime;
+            //check if we need to emit last forming candle ( or some fillers )
+            Scan(newCandle.Time);
 
             if (FormingCandle.IsDefault())
             {
-                //tbere isn't any currently forming candle, create one using new candle as generator
-                var opeTime = GetOpenTime(newCandle.CloseTime, Resolution);
-                FormingCandle = new Candlestick(opeTime, newCandle, Resolution);
-            }
-            if (topen >= FormingCandle.CloseTime || tclose > FormingCandle.CloseTime)
-            {
-                //old candle is ended, the new candle is already part of the next one
-                OnConsolidated?.Invoke(FormingCandle);
-                //use new candle as generator
-                var opeTime = GetOpenTime(newCandle.CloseTime, Resolution);
-                FormingCandle = new Candlestick(opeTime, newCandle, Resolution);
+                //there isn't any currently forming candle, create one using new candle as generator
+                var opeTime = CalculateFirstOpenTime(newCandle.CloseTime, Resolution);
+                FormingCandle.SetData(opeTime, Resolution, newCandle);
             }
             else
             {
                 //the new candle is part of the forming candle
                 FormingCandle.Merge(newCandle);
-                //check if candle is completed and emit it
-                if (tclose == FormingCandle.Time)
+            }
+            Debug.Assert(newCandle.OpenTime <= FormingCandle.CloseTime);
+            Debug.Assert(newCandle.Time <= FormingCandle.CloseTime);
+
+
+            //check if candle is last
+            if (newCandle.Time == FormingCandle.Time)
+            {
+                EmitCandle(ref FormingCandle);
+            }
+
+        }
+        public void Scan(DateTime timeNow)
+        {
+            //emit all missing candlesticks 
+            if (!LastEmittedCandle.IsDefault())
+            {
+                var timeIter = LastEmittedCandle.Time + Resolution + FinalCandleUpdateTimeout;
+                while (timeIter <= timeNow)
                 {
-                    OnConsolidated?.Invoke(FormingCandle);
-                    FormingCandle = new Candlestick();
+                    //if we have no data about forming candle then create a filler
+                    if (FormingCandle.IsDefault() || FormingCandle.Time < LastEmittedCandle.Time + Resolution)
+                        FormingCandle = Candlestick.GetFiller(LastEmittedCandle);
+                    EmitCandle(ref FormingCandle);
+
+                    timeIter += Resolution;
                 }
             }
         }
---
+++ Tests/SharpTrader.Tests/TestTradeBarConsolidator.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpTrader.Tests
+{
+
+    public class TestTradeBarConsolidator
+    {
+
+        public void SimpleTest()
+        {
+            Random rand = new Random(1);
+            TradeBarConsolidator consolidator = new TradeBarConsolidator(TimeSpan.FromHours(1));
+            var timeNow = new DateTime(2022, 01, 01, 11, 11, 0, 0, DateTimeKind.Utc);
+            consolidator.OnConsolidated += (obj) => Console.WriteLine($"Emitted {obj} at {timeNow:yyyy-MM-dd HH-mm}"); ;
+
+            Candlestick GetCandle(DateTime time) =>
+                new Candlestick()
+                {
+                    OpenTime = time.AddMinutes(-1),
+                    CloseTime = time,
+                    Open = rand.Next(1000),
+                    High = rand.Next(1000),
+                    Low = rand.Next(1000),
+                    Close = rand.Next(1000)
+                };
+
+            while (timeNow < new DateTime(2022, 01, 02))
+            {
+                if(timeNow.AddMinutes(1) >= new DateTime(2022, 01, 02))
+                { 
+                }
+                timeNow = timeNow.AddMinutes(1);
+                consolidator.Update(GetCandle(timeNow));
+            }
+
+            timeNow = timeNow.AddHours(7.2);
+
+            while (timeNow < new DateTime(2022, 01, 03))
+            {
+                timeNow = timeNow.AddMinutes(1); 
+                consolidator.Update(GetCandle(timeNow)); 
+            }
+        } 
+    }
+}
