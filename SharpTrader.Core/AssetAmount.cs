84889194059659f9e190ac9b9b0cd99ab90a2d36
F.Buldo <f.buldo@riello-ups.com>
2024-12-18 14:48:52 +0100

    [AssetAmount] Add an overload of Convert which allows to specify a price

--- SharpTrader.Core/AssetAmount.cs
+++ SharpTrader.Core/AssetAmount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 
@@ -34,19 +35,24 @@ namespace SharpTrader
             else
                 throw new ArgumentException("The symbol feed is doesn't correspond to the assets pair");
         }
-        public static decimal Convert(AssetAmount amount, string targetAsset, ISymbolFeed feed)
+        /// <summary>
+        /// Convert the amount to the target asset
+        /// Allows to specify a custom target price for the conversion
+        /// </summary>
+        public static decimal Convert(AssetAmount amount, string targetAsset, ISymbolFeed feed, decimal? target_price = null)
         {
             if (amount.Asset == targetAsset)
                 return amount.Amount;
             if (feed == null)
                 throw new ArgumentException("No feed provieded for the conversion");
             if (feed.Symbol.Asset == targetAsset && feed.Symbol.QuoteAsset == amount.Asset)
-                return amount.Amount / (decimal)feed.Ask;
+                return amount.Amount / (target_price == null ? (decimal)feed.Ask : target_price.Value);
             else if (feed.Symbol.QuoteAsset == targetAsset && feed.Symbol.Asset == amount.Asset)
-                return amount.Amount * (decimal)feed.Bid;
+                return amount.Amount * (target_price == null ? (decimal)feed.Bid : target_price.Value);
             else
                 throw new ArgumentException("The symbol feed is doesn't correspond to the assets pair");
         }
+
 
         public override string ToString()
         {
